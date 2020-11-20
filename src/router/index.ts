d65b7bf217b3086f41a2527c29a124c5e1434b23
Hoang Loi <stove2192@gmail.com>
2020-11-20 17:37:18 +0700

    push code base

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,19 +1,33 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import Home from '../views/Home.vue'
+import { createRouter, createWebHistory } from '@ionic/vue-router';
+import { RouteRecordRaw } from 'vue-router';
+import Tabs from '../views/Tabs.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    redirect: '/tabs/tab1'
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/tabs/',
+    component: Tabs,
+    children: [
+      {
+        path: '',
+        redirect: 'tab1'
+      },
+      {
+        path: 'tab1',
+        component: () => import('@/views/Tab1.vue')
+      },
+      {
+        path: 'tab2',
+        component: () => import('@/views/Tab2.vue')
+      },
+      {
+        path: 'tab3',
+        component: () => import('@/views/Tab3.vue')
+      }
+    ]
   }
 ]
 
---
+++ vue.config.js
@@ -0,0 +1,21 @@
+// const CopyPlugin = require("copy-webpack-plugin");
+// const path = require("path");
+
+module.exports = {
+    outputDir : 'www'
+//   configureWebpack: {
+//     plugins: [
+//         new CopyPlugin({
+//           patterns: [
+//             {
+//               from: 'dist/*',
+//               to: 'wwww/',
+//               transformPath(targetPath, absolutePath) {
+//                 return 'newPath';
+//               },
+//             },
+//           ],
+//         }),
+//       ],
+//   }
+};
